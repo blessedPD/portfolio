537b9f5ea392cda61093cbec32ac6870bd3489f1
blessedPD <115356350+blessedPD@users.noreply.github.com>
2022-10-22 22:03:58 +0900

    JS: Project filtering

--- main.js
+++ main.js
@@ -32,12 +32,6 @@ homeContactBtn.addEventListener('click',(event)=>{
     scrollIntoView('#Contact')
 });
 
-function scrollIntoView(selector){
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior:'smooth'});
-}
-
-
 //Make home slowly fade to transparent as the window scrolls down
 
 
@@ -64,3 +58,38 @@ document.addEventListener('scroll',()=>{
 arrowUp.addEventListener('click',()=>{
     scrollIntoView('#Home');
 });
+
+
+
+//Projects
+
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+
+workBtnContainer.addEventListener('click',(e)=> {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+
+    if (filter==null){
+        return;
+    }
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(()=> {
+        projects.forEach((project)=>{
+            if(filter==='*' || filter === project.dataset.type){
+                project.classList.remove('invisible');
+            }else{
+                project.classList.add('invisible');
+            }
+        })
+        projectContainer.classList.remove('anim-out');
+    },300);
+});
+
+
+////////////////
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:'smooth'});   
+}
