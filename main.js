9e7d57dab067deb60647aaa0d7f38696e8bf3f55
blessedPD <115356350+blessedPD@users.noreply.github.com>
2022-10-22 17:08:06 +0900

    JS: navbar menu click

--- main.js
+++ main.js
@@ -12,4 +12,38 @@ document.addEventListener('scroll', () => {
     }
 });
 
- 
+
+//Handle scrolling when tapping on hte navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click',(event)=>{
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link==null){
+        return;
+    }else{
+        const scrollTo = document.querySelector(link);
+        scrollTo.scrollIntoView({behavior:'smooth'});
+    }
+    
+});
+
+/*
+document.addEventListener('click',(event)=>{
+    let id = event.target.id;
+    let className = event.target.className;
+    console.log(id);
+
+    if (className="navbar__menu__item"){
+        if (id=="menu_item_0"){
+            
+        }else if (id=="menu_item_1"){
+            let el = document.getElementById("about");
+            
+            window.scrollTo(0,el.offsetTop-document.getElementById("navbar").offsetHeight);
+        }else if (id=="menu_item_2"){
+
+        }
+    }
+});
+*/
